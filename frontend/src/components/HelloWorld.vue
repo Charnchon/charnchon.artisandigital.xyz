6c0e80305a461c928cd273fe7a3f501a068da3a8
Charnchon <boatcharnchon@gmail.com>
2020-08-27 15:51:40 +0700

    add nuxt

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div>
+    <div class="navbar">
+      <div class="nav-component">
+        boat
+      </div>
+      <div class="nav-component">
+        
+      </div>
+
+    </div>
   </div>
 </template>
 
@@ -15,18 +23,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .navbar {
+
+  }
 </style>
